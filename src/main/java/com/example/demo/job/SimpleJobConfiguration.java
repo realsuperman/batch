f39ea5c155f7ea6b878cf5fc49acd7b3be4094b6
최성훈 <seonghun7304@naver.com>
2022-05-07 17:48:03 +0900

    spring batch code 제작중

--- src/main/java/com/example/demo/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/demo/job/SimpleJobConfiguration.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,15 +22,31 @@ public class SimpleJobConfiguration {
     @Bean
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob") // simpleJob이라는 배치 job 생성
-                .start(simpleStep1())
+                .start(simpleStep1(null))
+                .next(simpleStep2(null))
                 .build();
     }
 
     @Bean
-    public Step simpleStep1() {
-        return stepBuilderFactory.get("simpleStep1") // simpleStep1 이란 이름의 Batch Step을 생성
-                .tasklet((contribution, chunkContext) -> { // step 안에서 수행될 기능들을 명시
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep1")
+                .tasklet((contribution, chunkContext) -> {
                     log.info(">>>>> This is Step1");
+                    log.info(">>>>> requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                    //throw new IllegalArgumentException("fail");
+                })
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step2");
+                    log.info(">>>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
